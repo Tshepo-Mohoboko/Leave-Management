d88020d5ea2f7d07d12ed1037ef61dbc0698f13a
Tshepo Mohoboko <tshepomohobokolive@gmail.com>
2021-03-14 21:22:58 +0200

    leavemanagement using flask

---
+++ Documents/github/leavemanagement/src/main/java/spring/leavemanagement/leavemanagement/LeavemanagementApplication.java
@@ -0,0 +1,13 @@
+package spring.leavemanagement.leavemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+@SpringBootApplication
+public class LeavemanagementApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavemanagementApplication.class, args);
+	}
+
+}
---
+++ Documents/github/leavemanagement/src/main/java/spring/leavemanagement/leavemanagement/ProductRepository.java
@@ -0,0 +1,7 @@
+package spring.leavemanagement.leavemanagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
